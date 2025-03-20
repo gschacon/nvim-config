3b0835bf43648d06f0b19e331b2f7ebb69426a47
Guilherme Chacon <chacon.gsc@gmail.com>
2025-03-19 21:05:51 -0300

    testing fugitive push

--- lua/gschacon/plugins/fugitive.lua
+++ lua/gschacon/plugins/fugitive.lua
@@ -4,5 +4,6 @@ return { -- Manage git inside nvim
 		vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = "[G]it [S]tatus" })
 		vim.keymap.set("n", "<leader>ga", ":Git add .<CR>", { desc = "[G]it [A]dd ." })
 		vim.keymap.set("n", "<leader>gm", ":Git commit<CR>", { desc = "[G]it [C]ommit" })
+		vim.keymap.set("n", "<leader>gp", ":Git push<CR>", { desc = "[G]it [P]ush" })
 	end,
 }
