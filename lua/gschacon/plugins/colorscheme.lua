8a2468768676d32271dcf651595d38497add73ad
gschacon <chacon.gsc@gmail.com>
2025-03-22 22:20:38 -0300

    added transparent background for kitty

--- lua/gschacon/plugins/colorscheme.lua
+++ lua/gschacon/plugins/colorscheme.lua
@@ -37,7 +37,7 @@ return { -- Theme. Try :Telescope colorscheme
 		},
 	})
 		vim.cmd.colorscheme("catppuccin")
-		-- vim.api.nvim_set_hl(0, "Normal", { bg = "#000000" })
-	    -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#000000" })
+		vim.api.nvim_set_hl(0, "Normal", { bg = "#000000" })
+	    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#000000" })
 	end,
 }
