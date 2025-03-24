d3ea31b0a036d3b1fadb1fddbe918c380d3891b7
gschacon <chacon.gsc@gmail.com>
2025-03-24 05:02:58 -0300

    added Undotree description

--- lua/gschacon/plugins/undotree.lua
+++ lua/gschacon/plugins/undotree.lua
@@ -1,6 +1,6 @@
 return { -- Undotree to manage undos
 	"mbbill/undotree",
 	config = function()
-		vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+		vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, {desc = "Toggle [U]ndotree"})
 	end,
 }
